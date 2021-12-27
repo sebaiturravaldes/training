71eab0ff6d8897692ec9cb47fda20494e11c048f
Sebastián Iturra <iturra.sebastian@hotmail.com>
2021-12-27 13:09:17 -0300

    modified Example

--- react-pass-props/src/App/Example/index.js
+++ react-pass-props/src/App/Example/index.js
@@ -1,18 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export default function Example() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    //alert('El nuevo contador es: ' + count)
-    }, [count])
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+export default function Example(props) {
+  return <div>Hello, {props.name}</div>
 }
--- react-pass-props/src/App/index.js
+++ react-pass-props/src/App/index.js
@@ -1,10 +1,12 @@
 import './App.css';
 import List from './List';
+import Example from './Example';
 
 function App() {
   return (
     <div className="App">
       Soy la app
+      <Example name="Tanjiro" />
       <List />
     </div>
   );
